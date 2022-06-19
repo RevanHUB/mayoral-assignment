d20fcfb42499e95ce950500d8151875a462760c4
David <davidasir2020@gmail.com>
2022-06-19 12:04:00 +0000

    chore: commented the code, doing extra features

--- src/pages/components/exportProps.tsx
+++ src/pages/components/exportProps.tsx
@@ -1,3 +1,4 @@
+// Añadido para evitar el error al hacer el deploy de la aplicación:
 export async function getStaticProps() {
     const res = await fetch('http://localhost:3001/products');
     const data = await res.json();
